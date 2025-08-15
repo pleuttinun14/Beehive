548e773d378fba4d121f4875a371991e7df2d641
Pat <pleuttinunp@gmail.com>
2025-08-15 11:45:15 +0700

    GPS 30m
    Coding receives data from the GPS and sets it to loop every 30 minutes

--- Beehive/src/main.cpp
+++ Beehive/src/main.cpp
@@ -1,109 +1,86 @@
 #include <TinyGPS++.h>
-#include <ArduinoJson.h>
-#include <LoRa.h>
-#include <SPI.h>
-#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
 
-// Pin definitions
-#define GPS_RX 16   
-#define GPS_TX 17   
-#define LORA_SS 18  
-#define LORA_RST 14 
-#define LORA_DIO0 26
-#define LORA_BAND 923E6 
+// กำหนดพินสำหรับ GPS
+static const int RXPin = 4, TXPin = 3;
+static const int GPSBaud = 9600;
 
-// Device configuration
-#define DEVICE_ID "GPS_001"
-#define MIN_SATELLITES 4
-#define MAX_HDOP 10.0
-
+// ออบเจกต์ TinyGPS++
 TinyGPSPlus gps;
-HardwareSerial gpsSerial(1); 
 
-unsigned long lastSendTime = 0;
-const unsigned long interval = 20UL * 60UL * 1000UL; // 20 นาที
-unsigned long totalPacketsSent = 0;
+// Serial สำหรับ GPS
+SoftwareSerial ss(RXPin, TXPin);
 
-void setup() {
-  gpsSerial.begin(9600, SERIAL_8N1, GPS_RX, GPS_TX);
+// ตัวแปรเก็บค่าที่อ่านได้
+double latitude = 0.0;
+double longitude = 0.0;
+byte hour_gps = 0;
+byte minute_gps = 0;
+byte second_gps = 0;
+byte day_gps = 0;
+byte month_gps = 0;
+int year_gps = 0;
 
-  SPI.begin();
-  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
+void setup()
+{
+  Serial.begin(115200); // debug ถ้าจำเป็น
+  ss.begin(GPSBaud);
 
-  while (!LoRa.begin(LORA_BAND)) {
-    delay(1000);
-  }
-
-  LoRa.setTxPower(20);
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setCodingRate4(8);
-  LoRa.enableCrc();
-}
-
-void loop() {
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-  }
-  
-  if (millis() - lastSendTime >= interval) {
-    if (isValidGPSFix()) {
-      String jsonString = createJson(
-        gps.location.lat(),
-        gps.location.lng(),
-        gps.altitude.meters(),
-        gps.time.hour(),
-        gps.time.minute(),
-        gps.time.second(),
-        gps.satellites.value(),
-        gps.hdop.hdop(),
-        gps.speed.kmph()
-      );
-      sendLoRaPacket(jsonString);
-      totalPacketsSent++;
+  // รอจนกว่าจะได้ค่าจริงจาก GPS
+  while (true)
+  {
+    while (ss.available() > 0)
+    {
+      gps.encode(ss.read());
     }
-    lastSendTime = millis();
+
+    // ตรวจสอบว่าค่าพร้อมครบทุกอย่างหรือยัง
+    if (gps.location.isValid() &&
+        gps.time.isValid() &&
+        gps.date.isValid())
+    {
+      // อัปเดตตัวแปร
+      latitude = gps.location.lat();
+      longitude = gps.location.lng();
+      hour_gps = gps.time.hour();
+      minute_gps = gps.time.minute();
+      second_gps = gps.time.second();
+      day_gps = gps.date.day();
+      month_gps = gps.date.month();
+      year_gps = gps.date.year();
+
+      break; // ออกจาก while(true) เมื่อได้ค่าครบ
+    }
   }
 }
 
-bool isValidGPSFix() {
-  return gps.location.isValid() && 
-         gps.time.isValid() && 
-         gps.satellites.isValid() &&
-         gps.hdop.isValid() &&
-         gps.satellites.value() >= MIN_SATELLITES && 
-         gps.hdop.hdop() <= MAX_HDOP;
-}
-
-void sendLoRaPacket(String data) {
-  if (data.length() <= 200) {
-    LoRa.beginPacket();
-    LoRa.print(data);
-    LoRa.endPacket();
+void loop()
+{
+  // ใน loop() ก็สามารถอัปเดตค่าต่อได้ตามต้องการ
+  while (ss.available() > 0)
+  {
+    gps.encode(ss.read());
   }
-}
 
-String createJson(double lat, double lng, double alt, int hour, int minute, int second, 
-                  int satellites, double hdop, double speed) {
-  StaticJsonDocument<300> doc;
-  
-  doc["device_id"] = DEVICE_ID;
-  doc["timestamp"] = millis();
-  doc["latitude"] = serialized(String(lat, 6));
-  doc["longitude"] = serialized(String(lng, 6));
-  doc["altitude"] = serialized(String(alt, 1));
+  if (gps.location.isValid())
+  {
+    latitude = gps.location.lat();
+    longitude = gps.location.lng();
+  }
 
-  char timeStr[9];
-  sprintf(timeStr, "%02d:%02d:%02d", hour, minute, second);
-  doc["time_utc"] = timeStr;
-  
-  doc["satellites"] = satellites;
-  doc["hdop"] = serialized(String(hdop, 2));
-  doc["speed_kmh"] = serialized(String(speed, 1));
-  doc["packet_count"] = totalPacketsSent + 1;
-  
-  String jsonString;
-  serializeJson(doc, jsonString);
-  
-  return jsonString;
+  if (gps.time.isValid())
+  {
+    hour_gps = gps.time.hour();
+    minute_gps = gps.time.minute();
+    second_gps = gps.time.second();
+  }
+
+  if (gps.date.isValid())
+  {
+    day_gps = gps.date.day();
+    month_gps = gps.date.month();
+    year_gps = gps.date.year();
+  }
+
+  // ไม่มีการ print ออกมา
 }
